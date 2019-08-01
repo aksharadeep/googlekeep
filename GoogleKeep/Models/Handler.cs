8249ad524e0a423af2094171b6f68416e1fc5102
aksharadeep <aksharadeephr@gmail.com>
2019-08-01 21:15:23 +0530

    keep note

---
+++ GoogleKeep/Models/Handler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleKeep.Models
+{
+    public class Handler
+    {
+        public KeepNote keepNote = null;
+        public static List<KeepNote> keepNotes = new List<KeepNote>();
+
+        public void AddNote(KeepNote keepNote)
+        {
+            keepNotes.Add(keepNote);
+        }
+
+        public KeepNote UpdateTitle(int id, string title)
+        {
+            foreach (KeepNote item in keepNotes)
+            {
+                if (id == item.NoteId)
+                {
+                    item.NoteTitle = title;
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public void DeleteNote(int id)
+        {
+            foreach (KeepNote item in keepNotes)
+            {
+                if (id == item.NoteId)
+                    keepNotes.Remove(item);
+            }
+        }
+
+        public KeepNote GetNote(int id)
+        {
+            foreach (KeepNote item in keepNotes)
+            {
+                if (id == item.NoteId)
+                    return item;
+            }
+            return null;
+        }
+    }
+}
---
+++ GoogleKeep/Models/KeepNote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleKeep.Models
+{
+    public class KeepNote
+    {
+
+       public int NoteId { get; set; }
+       public string NoteTitle { get; set; }
+
+
+        public KeepNote(int noteId, string noteTitle)
+        {
+            this.NoteId = noteId;
+            this.NoteTitle = noteTitle;
+        }
+
+    }
+}
